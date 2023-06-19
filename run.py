61096675c15f36eaa1e9ff14ba21ba327f7d94e8
Sam Clearman <sam@samcelarman.com>
2023-06-19 13:50:37 -0700

    save games

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
+import json
 import modal
+import os
+import random
+from uuid import uuid4 as uuid
 
 from game import game
 
@@ -13,10 +17,21 @@ def random_game(_):
     return game.random_game()
 
 @stub.local_entrypoint()
-def main():
-    # Genrate training data
-    print('Generating random games...')
-    games = random_game.map(range(10))
-    for game in games:
-        print(game.blobs[-1])
-        print("")
+def main(games_path: str):
+    n_games = 10
+
+    # Load games
+    print(f'Loading games from {games_path}...')
+    games = []
+    for game_file in os.listdir(games_path)[:n_games]:
+        with open(os.path.join(games_path, game_file), 'r') as f:
+            games.append(game.load_game(json.load(f)))
+
+    # Generate games
+    if (len(games) < n_games):
+        print(f'Generating {n_games - len(games)} random games...')
+        new_games = random_game.map(range(n_games - len(games)))
+        for g in new_games:
+            with open(os.path.join(games_path, str(uuid()) + '.game'), 'w') as f:
+                f.write(g.json)
+        games += new_games
